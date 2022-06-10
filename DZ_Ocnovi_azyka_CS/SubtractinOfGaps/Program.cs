0d74c8ca0f79019ed6740b7481f398a39236168d
Roman <AverkievRE@yandex.ru>
2022-06-10 11:52:09 +0300

    Реализация проэкта SubtractinOfGaps

---
+++ DZ_Ocnovi_azyka_CS/SubtractinOfGaps/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+   Написать программу, которая считывает символы с клавиатуры, пока не 
+будет введена точка. Программа должна сосчитать количество введенных 
+пользователем пробелов.
+ */
+
+using System;
+
+namespace SubtractinOfGaps
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            string? str;
+            do
+            {
+                Console.WriteLine("Введите символ");
+                Console.WriteLine(" . - завершение программы ");
+
+                str = Console.ReadLine();
+
+                if (str.Length > 1)
+                {
+                    Console.WriteLine("Введено больше одного символа");
+                }
+                else if (str == " ")
+                {
+                    ++count;
+                }
+
+
+            } while (str != ".");
+
+            Console.WriteLine($"Введено пробелов: {count}");
+        }
+
+
+    }
+}
